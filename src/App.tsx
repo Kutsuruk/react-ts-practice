99293e92954e6b821a3b4c3bda605b271a454dda
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2022-09-13 17:12:06 +0300

    Type useState hook. Update type with future changing value of state.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Oscar from "./components/Oscar";
 import Button from "./components/Button";
 import Input from "./components/Input";
 import Container from "./components/Container";
+import LoggedIn from "./components/state/LoggedIn";
 
 function App() {
     const person = {
@@ -31,7 +32,7 @@ function App() {
 
   return (
     <div>
-        <Container styles={{ border: '1px solid black', padding: '1rem', textAlign: 'center' }} />
+        <LoggedIn />
     </div>
   );
 }
---
+++ src/components/state/User.tsx
@@ -0,0 +1,22 @@
+import {FC, useState} from "react";
+
+type AuthUser = {
+    name: string,
+    email: string
+}
+
+const User:FC<AuthUser> = () => {
+    const [user, setUser] = useState<null | AuthUser>(null)
+
+    const handleLogin = () => setUser({name: 'Vishwas', email: 'vishwas@mail.com'})
+    const handleLogout = () => setUser(null)
+
+    return(
+        <div>
+            <button>Login</button>
+            <button>Logout</button>
+            <div>User name is {user?.name}</div>
+            <div>User e-mail is {user?.email}</div>
+        </div>
+    )
+}
