6a129b590a63a0a2a59cf9a7d6474fea895ca18b
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2022-09-13 17:16:31 +0300

    Using type to applying for state initial type using as keyword.

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,17 +6,17 @@ type AuthUser = {
 }
 
 const User:FC<AuthUser> = () => {
-    const [user, setUser] = useState<null | AuthUser>(null)
+    const [user, setUser] = useState<AuthUser>({} as AuthUser)
 
     const handleLogin = () => setUser({name: 'Vishwas', email: 'vishwas@mail.com'})
-    const handleLogout = () => setUser(null)
+    // const handleLogout = () => setUser(null)
 
     return(
         <div>
             <button>Login</button>
             <button>Logout</button>
-            <div>User name is {user?.name}</div>
-            <div>User e-mail is {user?.email}</div>
+            <div>User name is {user.name}</div>
+            <div>User e-mail is {user.email}</div>
         </div>
     )
 }
